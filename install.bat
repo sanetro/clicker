2bf44a79ab4f4f6d74346c35f705e3b69f87a643
Bartek <50074470+sanetro@users.noreply.github.com>
2021-02-27 16:30:37 +0100

    fix

--- install.bat
+++ install.bat
@@ -1,18 +1,8 @@
 @echo off
 
-python --version 2>NUL
-if not errorlevel 1 goto errorNoPython
-if errorlevel 1 goto installModules
-
-
-goto:eof
-
-:errorNoPython
-echo.
-echo Error^: Python not installed
-
-:installModules
 pip install pygame
-pip install sys
-pip install os
-pip install win32api
+pip install pypiwin32
+
+color a 
+echo Installation completed 
+pause
